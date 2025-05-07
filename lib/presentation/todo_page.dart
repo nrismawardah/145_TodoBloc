18de96f11ad46646ba28c11366d1f022e5aa74f3
nrismawardah <nrismawardah@gmail.com>
2025-05-07 21:09:45 +0700

    Membuat button untuk memilih tanggal

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -33,6 +33,24 @@ class TodoPage extends StatelessWidget {
                           return Text('No date selected');
                         },
                       ),
+                      SizedBox(height: 16.0),
+                      ElevatedButton(
+                        onPressed: () {
+                          showDatePicker(
+                            context: context,
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2100),
+                          ).then((selectedDate) {
+                            if (selectedDate != null) {
+                              context.read<TodoBloc>().add(
+                                TodoSelectDate(date: selectedDate),
+                              );
+                            }
+                          });
+                        },
+                        child: Text('Select Date'),
+                      ),
                     ],
                   ),
                 ],
