e5d63fcdeebe6ecc60a0f4c1bac718336860db36
nrismawardah <nrismawardah@gmail.com>
2025-05-07 21:26:13 +0700

    Membuat bloc builder

--- lib/presentation/todo_page.dart
+++ lib/presentation/todo_page.dart
@@ -95,6 +95,27 @@ class TodoPage extends StatelessWidget {
                   ],
                 ),
               ),
+              SizedBox(height: 16.0),
+              Expanded(
+                child: BlocBuilder<TodoBloc, TodoState>(
+                  builder: (context, state) {
+                    if (state is TodoLoading) {
+                      return Center(child: CircularProgressIndicator());
+                    } else if (state is TodoLoaded) {
+                      if (state.todos.isEmpty) {
+                        return Center(child: Text('Todo list is empty'));
+                      }
+                      return ListView.builder(
+                        itemCount: state.todos.length,
+                        itemBuilder: (context, index) {
+                          final todo = state.todos[index];
+                          return;
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
             ],
           ),
         ),
